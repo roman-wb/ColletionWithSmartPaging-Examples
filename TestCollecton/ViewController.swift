a14598b98b7e36b7b03298a1e6a4c191c82d731d
Roman Dobynda <roman-wb@yandex.ru>
2020-05-04 15:32:00 +0300

    Initial Commit

---
+++ TestCollecton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCollecton
+//
+//  Created by roman on 04.05.2020.
+//  Copyright © 2020 ROMAN DOBYNDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
